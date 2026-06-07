be0227d2bd8f15df0eee1da1a8ed1614c88ca879
Fernando Barboza <fernando.p.barboza@gmail.com>
2026-06-07 12:54:19 -0300

    Update-Barbozagram-Add PWA features

---
+++ barbozagram/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"bcc4686d4da626b38131a564c9f51cfe","url":"assets/css/style.css"},{"revision":"61cde632d96e08607815d7dcac05619a","url":"assets/js/main.js"},{"revision":"ec0d76e9c8894338c4f010f1b5efbf71","url":"index_bkp.html"},{"revision":"dae518f6b7be73767f864c1912d3df7e","url":"index.html"}]);
